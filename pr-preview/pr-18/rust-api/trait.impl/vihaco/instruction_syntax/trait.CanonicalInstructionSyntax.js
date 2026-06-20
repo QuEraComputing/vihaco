d200d2aa1fd79516e34aad5e8ca536d45a3f2e44
Roger-luo <Roger-luo@users.noreply.github.com>
2026-06-20 00:21:44 +0000

    Remove preview for PR 18 🛬

--- pr-preview/pr-18/rust-api/trait.impl/vihaco/instruction_syntax/trait.CanonicalInstructionSyntax.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["vihaco_cpu",[["impl CanonicalInstructionSyntax for <a class=\"enum\" href=\"vihaco_cpu/enum.Instruction.html\" title=\"enum vihaco_cpu::Instruction\">Instruction</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[173]}
--- pr-preview/pr-18/rust-api/vihaco/machine/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"trait":["FrameMemory","GetProgramGlobal","ProgramCounter","StackFrame","StackMemory","Stdout"]};
