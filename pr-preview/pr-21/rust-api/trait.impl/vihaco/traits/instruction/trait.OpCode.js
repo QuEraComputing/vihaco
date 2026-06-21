a8a8ec5abd64c5abb17a145d03e242dfb7ab258f
Roger-luo <Roger-luo@users.noreply.github.com>
2026-06-21 03:03:11 +0000

    Remove preview for PR 21 🛬

--- pr-preview/pr-21/rust-api/trait.impl/vihaco/traits/instruction/trait.OpCode.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["vihaco",[]],["vihaco_cpu",[["impl OpCode for <a class=\"enum\" href=\"vihaco_cpu/enum.Instruction.html\" title=\"enum vihaco_cpu::Instruction\">Instruction</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[13,154]}
--- pr-preview/pr-21/rust-api/vihaco/runtime/sidebar-items.js
+++
@@ -1,1 +0,0 @@
-window.SIDEBAR_ITEMS = {"fn":["expect_exactly_one_effect"],"struct":["CompositeMetadata"],"trait":["EffectSink","GeneratedComponent","Message","Observe"]};
