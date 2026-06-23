7996bdd0950eb38091dea1fa67e8319ff03de1e6
Roger-luo <Roger-luo@users.noreply.github.com>
2026-06-23 01:51:41 +0000

    docs: deploy 888f3abd8e7768c2dfba35a2eb0ac796bdb7b397 888f3abd8e7768c2dfba35a2eb0ac796bdb7b397

--- rust-api/trait.impl/vihaco/traits/machine/trait.StackMemory.js
+++ rust-api/trait.impl/vihaco/traits/machine/trait.StackMemory.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["vihaco_cpu",[["impl <a class=\"trait\" href=\"vihaco/traits/machine/trait.StackMemory.html\" title=\"trait vihaco::traits::machine::StackMemory\">StackMemory</a> for <a class=\"struct\" href=\"vihaco_cpu/struct.CPU.html\" title=\"struct vihaco_cpu::CPU\">CPU</a>",0]]]]);
+    const implementors = Object.fromEntries([["vihaco_cpu",[["impl StackMemory for <a class=\"struct\" href=\"vihaco_cpu/struct.CPU.html\" title=\"struct vihaco_cpu::CPU\">CPU</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[270]}
+//{"start":59,"fragment_lengths":[140]}
