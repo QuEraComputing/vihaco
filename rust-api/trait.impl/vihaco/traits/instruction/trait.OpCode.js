3ed69a19d3ee74d73231021b16036505221c68b8
Roger-luo <Roger-luo@users.noreply.github.com>
2026-06-23 00:52:00 +0000

    docs: deploy 6d12cb8270e0f637efa24f6559bc9bfd74a768a3 6d12cb8270e0f637efa24f6559bc9bfd74a768a3

--- rust-api/trait.impl/vihaco/traits/instruction/trait.OpCode.js
+++ rust-api/trait.impl/vihaco/traits/instruction/trait.OpCode.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["vihaco",[]],["vihaco_cpu",[["impl OpCode for <a class=\"enum\" href=\"vihaco_cpu/enum.Instruction.html\" title=\"enum vihaco_cpu::Instruction\">Instruction</a>",0]]]]);
+    const implementors = Object.fromEntries([["vihaco",[]],["vihaco_cpu",[["impl <a class=\"trait\" href=\"vihaco/traits/instruction/trait.OpCode.html\" title=\"trait vihaco::traits::instruction::OpCode\">OpCode</a> for <a class=\"enum\" href=\"vihaco_cpu/enum.Instruction.html\" title=\"enum vihaco_cpu::Instruction\">Instruction</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[13,154]}
+//{"start":59,"fragment_lengths":[13,282]}
