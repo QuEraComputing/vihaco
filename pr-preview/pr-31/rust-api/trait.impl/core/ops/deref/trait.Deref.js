cfa071efd039bf19da7013c76de7c72708443701
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-06-25 18:21:13 +0000

    Deploy preview for PR 31 🛫

---
+++ pr-preview/pr-31/rust-api/trait.impl/core/ops/deref/trait.Deref.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["vihaco",[["impl&lt;C&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"vihaco/binary/struct.ContextHandle.html\" title=\"struct vihaco::binary::ContextHandle\">ContextHandle</a>&lt;C&gt;",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[322]}
---
+++ pr-preview/pr-31/rust-api/vihaco/loader/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["LoadInput","ModuleProgramLoader","ProgramLoader"],"trait":["LoadSection"]};
