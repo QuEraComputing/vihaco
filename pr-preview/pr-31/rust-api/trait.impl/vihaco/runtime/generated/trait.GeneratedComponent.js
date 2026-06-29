1cd07c6cbf0d0daad63a590cc910493e67f0b54f
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-06-29 20:10:16 +0000

    Deploy preview for PR 31 🛫

--- pr-preview/pr-31/rust-api/trait.impl/vihaco/runtime/generated/trait.GeneratedComponent.js
+++ pr-preview/pr-31/rust-api/trait.impl/vihaco/runtime/generated/trait.GeneratedComponent.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["vihaco_cpu",[["impl <a class=\"trait\" href=\"vihaco/runtime/generated/trait.GeneratedComponent.html\" title=\"trait vihaco::runtime::generated::GeneratedComponent\">GeneratedComponent</a> for <a class=\"struct\" href=\"vihaco_cpu/struct.CPU.html\" title=\"struct vihaco_cpu::CPU\">CPU</a>",0]]]]);
+    const implementors = Object.fromEntries([["vihaco",[]],["vihaco_cpu",[["impl <a class=\"trait\" href=\"vihaco/runtime/generated/trait.GeneratedComponent.html\" title=\"trait vihaco::runtime::generated::GeneratedComponent\">GeneratedComponent</a> for <a class=\"struct\" href=\"vihaco_cpu/struct.CPU.html\" title=\"struct vihaco_cpu::CPU\">CPU</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[297]}
+//{"start":59,"fragment_lengths":[13,298]}
