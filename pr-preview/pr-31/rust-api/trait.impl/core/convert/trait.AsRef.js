7f15747a9a77d8a4746cd7a94ac97e4605ac94c8
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-06-30 15:45:23 +0000

    Deploy preview for PR 31 🛫

--- pr-preview/pr-31/rust-api/trait.impl/core/convert/trait.AsRef.js
+++ pr-preview/pr-31/rust-api/trait.impl/core/convert/trait.AsRef.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["vihaco",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"enum\" href=\"vihaco/program/value/enum.Type.html\" title=\"enum vihaco::program::value::Type\">Type</a>&gt; for <a class=\"enum\" href=\"vihaco/program/value/enum.Type.html\" title=\"enum vihaco::program::value::Type\">Type</a>",0]]]]);
+    const implementors = Object.fromEntries([["vihaco",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/convert/trait.AsRef.html\" title=\"trait core::convert::AsRef\">AsRef</a>&lt;<a class=\"enum\" href=\"vihaco/program/value/enum.Type.html\" title=\"enum vihaco::program::value::Type\">Type</a>&gt; for <a class=\"enum\" href=\"vihaco/program/value/enum.Type.html\" title=\"enum vihaco::program::value::Type\">Type</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
--- pr-preview/pr-31/rust-api/trait.impl/core/ops/deref/trait.Deref.js
+++ pr-preview/pr-31/rust-api/trait.impl/core/ops/deref/trait.Deref.js
@@ -1,5 +1,5 @@
 (function() {
-    const implementors = Object.fromEntries([["vihaco",[["impl&lt;C&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.0/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"vihaco/binary/struct.ContextHandle.html\" title=\"struct vihaco::binary::ContextHandle\">ContextHandle</a>&lt;C&gt;",0]]]]);
+    const implementors = Object.fromEntries([["vihaco",[["impl&lt;C&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/1.96.1/core/ops/deref/trait.Deref.html\" title=\"trait core::ops::deref::Deref\">Deref</a> for <a class=\"struct\" href=\"vihaco/binary/struct.ContextHandle.html\" title=\"struct vihaco::binary::ContextHandle\">ContextHandle</a>&lt;C&gt;",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
