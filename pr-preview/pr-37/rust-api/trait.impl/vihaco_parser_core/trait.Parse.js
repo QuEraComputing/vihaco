3718da437555de9c3e4098b541cfc2749d3d3cc1
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-07-08 15:45:25 +0000

    Deploy preview for PR 37 🛫

---
+++ pr-preview/pr-37/rust-api/trait.impl/vihaco_parser_core/trait.Parse.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["vihaco",[["impl&lt;'src, I&gt; <a class=\"trait\" href=\"vihaco_parser_core/trait.Parse.html\" title=\"trait vihaco_parser_core::Parse\">Parse</a>&lt;'src&gt; for <a class=\"struct\" href=\"vihaco/syntax/struct.ParsedFunction.html\" title=\"struct vihaco::syntax::ParsedFunction\">ParsedFunction</a>&lt;I&gt;<div class=\"where\">where\n    I: <a class=\"trait\" href=\"vihaco_parser_core/trait.Parse.html\" title=\"trait vihaco_parser_core::Parse\">Parse</a>&lt;'src&gt; + 'src,</div>",0]]],["vihaco_cpu",[["impl&lt;'src&gt; <a class=\"trait\" href=\"vihaco_parser_core/trait.Parse.html\" title=\"trait vihaco_parser_core::Parse\">Parse</a>&lt;'src&gt; for <a class=\"enum\" href=\"vihaco_cpu/enum.Instruction.html\" title=\"enum vihaco_cpu::Instruction\">Instruction</a>",0]]],["vihaco_parser_core",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[492,287,26]}
