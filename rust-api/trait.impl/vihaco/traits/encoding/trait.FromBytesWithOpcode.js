d784f4e5ce92f9433dc206e041c6b5f8f300b60c
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-07-29 19:43:19 +0000

    docs: deploy 2c626635d358592431e624a3aaf9cc41514ee215 2c626635d358592431e624a3aaf9cc41514ee215

--- rust-api/trait.impl/vihaco/traits/encoding/trait.FromBytesWithOpcode.js
+++ rust-api/trait.impl/vihaco/traits/encoding/trait.FromBytesWithOpcode.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["vihaco_cpu",[["impl <a class=\"trait\" href=\"vihaco/traits/encoding/trait.FromBytesWithOpcode.html\" title=\"trait vihaco::traits::encoding::FromBytesWithOpcode\">FromBytesWithOpcode</a> for <a class=\"enum\" href=\"vihaco_cpu/enum.RuntimeInstruction.html\" title=\"enum vihaco_cpu::RuntimeInstruction\">RuntimeInstruction</a>",0]]]]);
+    const implementors = Object.fromEntries([["vihaco_cpu",[["impl FromBytesWithOpcode for <a class=\"enum\" href=\"vihaco_cpu/enum.RuntimeInstruction.html\" title=\"enum vihaco_cpu::RuntimeInstruction\">RuntimeInstruction</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[335]}
+//{"start":59,"fragment_lengths":[187]}
