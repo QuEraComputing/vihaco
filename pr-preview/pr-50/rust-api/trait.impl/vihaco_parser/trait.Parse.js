0066399ccc660b82d7272afc425c5b51c2f38a2a
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-08-04 17:12:30 +0000

    Remove preview for PR 50 🛬

--- pr-preview/pr-50/rust-api/trait.impl/vihaco_parser/trait.Parse.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["vihaco_cpu",[["impl&lt;'__vihaco_src&gt; <a class=\"trait\" href=\"vihaco_parser/trait.Parse.html\" title=\"trait vihaco_parser::Parse\">Parse</a>&lt;'__vihaco_src&gt; for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceInstruction.html\" title=\"enum vihaco_cpu::SurfaceInstruction\">SurfaceInstruction</a>",0],["impl&lt;'__vihaco_src&gt; <a class=\"trait\" href=\"vihaco_parser/trait.Parse.html\" title=\"trait vihaco_parser::Parse\">Parse</a>&lt;'__vihaco_src&gt; for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceType.html\" title=\"enum vihaco_cpu::SurfaceType\">SurfaceType</a>",0],["impl&lt;'__vihaco_src&gt; <a class=\"trait\" href=\"vihaco_parser/trait.Parse.html\" title=\"trait vihaco_parser::Parse\">Parse</a>&lt;'__vihaco_src&gt; for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceValue.html\" title=\"enum vihaco_cpu::SurfaceValue\">SurfaceValue</a>",0]]],["vihaco_parser",[]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[874,21]}
