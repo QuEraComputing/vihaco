0815bd123ee01580175f604630a52b7181542134
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-08-04 17:09:37 +0000

    Deploy preview for PR 50 🛫

--- pr-preview/pr-50/rust-api/trait.impl/vihaco_parser/trait.SurfaceInstruction.js
+++ pr-preview/pr-50/rust-api/trait.impl/vihaco_parser/trait.SurfaceInstruction.js
@@ -1,9 +1,9 @@
 (function() {
-    const implementors = Object.fromEntries([["vihaco_cpu",[["impl SurfaceInstruction for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceInstruction.html\" title=\"enum vihaco_cpu::SurfaceInstruction\">SurfaceInstruction</a>",0]]]]);
+    const implementors = Object.fromEntries([["vihaco_cpu",[["impl <a class=\"trait\" href=\"vihaco_parser/trait.SurfaceInstruction.html\" title=\"trait vihaco_parser::SurfaceInstruction\">SurfaceInstruction</a> for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceInstruction.html\" title=\"enum vihaco_cpu::SurfaceInstruction\">SurfaceInstruction</a>",0]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":59,"fragment_lengths":[186]}
+//{"start":59,"fragment_lengths":[312]}
