735dc60ab8f5493c7fda39b1dd5bb5fc04e2bbe0
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-08-14 16:33:46 +0000

    Deploy preview for PR 57 🛫

---
+++ pr-preview/pr-57/rust-api/trait.impl/vihaco_runtime/marker/trait.Message.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["vihaco",[]],["vihaco_cpu",[["impl Message for <a class=\"enum\" href=\"vihaco_cpu/enum.CPUMessage.html\" title=\"enum vihaco_cpu::CPUMessage\">CPUMessage</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[13,152]}
