87abdf4079362d06c78cdf53e6b62351706bcda7
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-08-27 13:35:04 +0000

    Deploy preview for PR 88 🛫

---
+++ pr-preview/pr-88/rust-api/trait.impl/core/cmp/trait.PartialOrd.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["vihaco_parser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.BareToken.html\" title=\"struct vihaco_parser::impls::BareToken\">BareToken</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.Ident.html\" title=\"struct vihaco_parser::impls::Ident\">Ident</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.98.0/core/cmp/trait.PartialOrd.html\" title=\"trait core::cmp::PartialOrd\">PartialOrd</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.QuotedString.html\" title=\"struct vihaco_parser::impls::QuotedString\">QuotedString</a>",0]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[898]}
