cf1cd8c496f6f166763435d096a8ecf0e21f2bed
kaihsin <kaihsin@users.noreply.github.com>
2026-09-14 15:22:14 +0000

    Remove preview for PR 96 🛬

--- pr-preview/pr-96/rust-api/trait.impl/vihaco_parser/trait.SurfaceInstruction.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["vihaco_cpu",[["impl <a class=\"trait\" href=\"vihaco_parser/trait.SurfaceInstruction.html\" title=\"trait vihaco_parser::SurfaceInstruction\">SurfaceInstruction</a> for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceInstruction.html\" title=\"enum vihaco_cpu::SurfaceInstruction\">Instruction</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[305]}
