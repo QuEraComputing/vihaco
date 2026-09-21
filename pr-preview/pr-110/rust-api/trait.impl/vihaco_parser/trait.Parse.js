3fd2e36a7d8123c0f2ab26cc07746d3f81f141a8
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-09-21 18:06:33 +0000

    Deploy preview for PR 110 🛫

---
+++ pr-preview/pr-110/rust-api/trait.impl/vihaco_parser/trait.Parse.js
@@ -0,0 +1,9 @@
+(function() {
+    const implementors = Object.fromEntries([["vihaco",[]],["vihaco_cpu",[["impl&lt;'__vihaco_src&gt; Parse&lt;'__vihaco_src&gt; for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceInstruction.html\" title=\"enum vihaco_cpu::SurfaceInstruction\">Instruction</a>",0],["impl&lt;'__vihaco_src&gt; Parse&lt;'__vihaco_src&gt; for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceType.html\" title=\"enum vihaco_cpu::SurfaceType\">SurfaceType</a>",0],["impl&lt;'__vihaco_src&gt; Parse&lt;'__vihaco_src&gt; for <a class=\"enum\" href=\"vihaco_cpu/enum.SurfaceValue.html\" title=\"enum vihaco_cpu::SurfaceValue\">SurfaceValue</a>",0]]],["vihaco_parser",[]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":59,"fragment_lengths":[13,568,21]}
