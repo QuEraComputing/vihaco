7888b0613a7cff098f844e9d354148b60572d246
Rob Patterson <rpatterson@quera.com>
2026-09-22 12:03:49 -0400

    Removed example from `Execute<I>` doc

--- crates/vihaco-runtime/src/execute.rs
+++ crates/vihaco-runtime/src/execute.rs
@@ -11,38 +11,6 @@ use crate::Component;
 ///
 /// The message should be created by the composite and passed into
 /// [`Execute::execute`].
-///
-/// # Example
-///
-/// ```
-/// use vihaco_runtime::{Component, Execute};
-/// use std::convert::Infallible;
-///
-/// // ALU is a stateless component
-/// struct ALU;
-/// impl Component for ALU {}
-///
-/// // Add is an instruction that takes no arguments;
-/// // useful for a stack machine
-/// struct Add;
-///
-/// struct BinaryOperands<L, R>(L, R);
-/// struct ALUResult<T>(T);
-///
-/// impl Execute<Add> for ALU {
-///     type Message = BinaryOperands<u32, u32>;
-///     type Effect = ALUResult<u32>;
-///     type Error = Infallible;
-///
-///     fn execute(
-///         &mut self,
-///         instruction: &Add,
-///         message: BinaryOperands<u32, u32>,
-///     ) -> Result<ALUResult<u32>, Infallible> {
-///         Ok(message.0.wrapping_add(message.1))
-///     }
-/// }
-/// ```
 pub trait Execute<I>
 where
     Self: Component,
