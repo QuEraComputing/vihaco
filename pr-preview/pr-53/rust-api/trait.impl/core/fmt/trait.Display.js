fc87612a74632f5ab009a7addfd318add90da5e0
robpatterson13 <robpatterson13@users.noreply.github.com>
2026-09-22 16:11:26 +0000

    Remove preview for PR 53 🛬

--- pr-preview/pr-53/rust-api/trait.impl/core/fmt/trait.Display.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["vihaco_cpu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"enum\" href=\"vihaco_cpu/enum.RuntimeInstruction.html\" title=\"enum vihaco_cpu::RuntimeInstruction\">RuntimeInstruction</a>",0]]],["vihaco_parser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.BareToken.html\" title=\"struct vihaco_parser::impls::BareToken\">BareToken</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.Ident.html\" title=\"struct vihaco_parser::impls::Ident\">Ident</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/fmt/trait.Display.html\" title=\"trait core::fmt::Display\">Display</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.QuotedString.html\" title=\"struct vihaco_parser::impls::QuotedString\">QuotedString</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[303,872]}
--- pr-preview/pr-53/rust-api/trait.impl/core/hash/trait.Hash.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    const implementors = Object.fromEntries([["vihaco_parser",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.BareToken.html\" title=\"struct vihaco_parser::impls::BareToken\">BareToken</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.Ident.html\" title=\"struct vihaco_parser::impls::Ident\">Ident</a>",0],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.97.1/core/hash/trait.Hash.html\" title=\"trait core::hash::Hash\">Hash</a> for <a class=\"struct\" href=\"vihaco_parser/impls/struct.QuotedString.html\" title=\"struct vihaco_parser::impls::QuotedString\">QuotedString</a>",0]]]]);
-    if (window.register_implementors) {
-        window.register_implementors(implementors);
-    } else {
-        window.pending_implementors = implementors;
-    }
-})()
-//{"start":59,"fragment_lengths":[850]}
